f401783ad593288fbcdef0684580ae96ba0a1c2d
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-09-27 15:51:05 -0400

    remove done

--- warmup/sorted_points.c
+++ warmup/sorted_points.c
@@ -7,8 +7,13 @@
 
 struct sorted_points {
 	/* you can define this struct to have whatever fields you want. */
+	struct points *head;
+	int size;
+};
+
+struct points{
 	struct point *pt;
-	struct sorted_points *next;
+	struct points *next;
 };
 struct sorted_points *
 sp_init()
@@ -17,8 +22,8 @@ sp_init()
 
 	sp = (struct sorted_points *)malloc(sizeof(struct sorted_points));
 	assert(sp);
-	sp->pt=NULL;
-	sp->next=NULL;
+	sp->head=NULL;
+	sp->size=0;
 	//TBD();
 
 	return sp;
@@ -28,20 +33,23 @@ void
 sp_destroy(struct sorted_points *sp)
 {
 	//TBD();
-	while(sp!=NULL){
-		struct sorted_points *tmp;
-		tmp=sp;
-		sp=sp->next;
-		free(tmp->pt);
-		free(tmp->next);
+	struct points *cur=sp->head;
+	while(cur!=NULL){
+		struct points* tmp=cur->next;
+		free(cur->pt);
+		free(cur->next);
+		free(cur);
+		cur=tmp;
 	}
+	sp->size=0;
+	free(sp->head);
 	free(sp);
 }
 
 int
 sp_add_point(struct sorted_points *sp, double x, double y)
 {
-	TBD();
+	//TBD();
 	/*struct point *p;
 	p=(struct point *)malloc(sizeof(struct point));
 	p->x=x;
@@ -65,30 +73,52 @@ sp_remove_first(struct sorted_points *sp, struct point *ret)
 /* Remove the first point from the sorted list.  Caller provides a pointer to a
  * Point where this procedure stores the values of that point. Returns 1 on
  * success and 0 on failure (empty list). */
-	if(sp==NULL){
-		ret=NULL;
+ printf("%p\n",ret );
+	if(sp->head==NULL){
 		return 	0;
 	}
 	else{
-		if(sp->pt!=NULL){
-			ret=malloc(sizeof(struct point));
-			point_set(ret,sp->pt->x,sp->pt->y);
-		}
-		
-		free(sp->pt);
-		free(sp->next);
-		free(sp);
+		point_set(ret,sp->head->pt->x,sp->head->pt->y);
+		struct points *tmp=sp->head->next;
+		free(sp->head->pt);
+		free(sp->head->next);
+		free(sp->head);
+		sp->head=tmp;
+		sp->size-=1;
 		return 1;
-	}
 
-	
+	}
 }
 
 int
 sp_remove_last(struct sorted_points *sp, struct point *ret)
 {
-	TBD();
-	return 0;
+/* Remove the last point from the sorted list, storing its value in
+ * *ret. Returns 1 on success and 0 on failure (empty list). */
+	//TBD();
+	if(sp->size==0){
+		return 0;
+	}
+	else if(sp->size==1){
+		free(sp->head->pt);
+		free(sp->head->next);
+		free(sp->head);
+		sp->size=0;
+		return 1;
+	}
+	else{
+		struct points *node=sp->head;
+		int i=2;
+		while(i<sp->size){
+			node=node->next;
+			i++;
+		}
+		free(node->next->pt);
+		free(node->next->next);
+		free(node->next);
+		sp->size-=1;
+		return 1;
+	}
 }
 
 int
@@ -98,46 +128,32 @@ sp_remove_by_index(struct sorted_points *sp, int index, struct point *ret)
  * its value in *ret. Returns 1 on success and 0 on failure (too short list).
  * The first item on the list is at index 0. */
  //use a fake head to eliminate corner cases
+ 	if(index>=sp->size||index<0){
+ 		return 0;
+ 	}
+ 	else{
+ 		if(sp->size==1){
+ 			free(sp->head->pt);
+ 			free(sp->head->next);
+ 			free(sp->head);
+ 		}
+ 		else{
+ 			struct points *node=sp->head;
+ 			int i=0;
+ 			while(i<index-1){
+ 				node=node->next;
+ 				i++;
+ 			}
+ 			struct points* tmp=node->next;
+ 			node->next=node->next->next;
+ 			free(tmp->pt);
+ 			free(tmp->next);
+ 			free(tmp);
+ 		}
+ 		sp->size-=1;
+ 		return 1;
+ 	}
 
-
-	struct sorted_points *fakeHead=malloc(sizeof(struct sorted_points));
-	fakeHead->next=sp;
-	struct sorted_points *cur=fakeHead;
-	int i=0;
-	int flag=0;
-	while(cur!=NULL){
-		if(i==index){
-			if(cur->next==NULL){
-				flag=0;
-				break;
-			}		
-			else{
-				struct sorted_points *tmp=cur->next;
-				cur->next=cur->next->next;
-				if(tmp->pt!=NULL){
-
-					ret=malloc(sizeof(struct point));
-					point_set(ret,tmp->pt->x,tmp->pt->y);
-				}	
-				free(tmp->pt);
-				free(tmp->next);
-				free(tmp);
-				flag=1;
-				break;
-
-			}
-		}
-		else{
-			cur=cur->next;
-			i++;
-		}
-	}
-	printf("%p\n",ret );
-	if(!flag)
-		free(ret);
-	//printf("x=%f y=%f \n",ret->x,ret->y );
-	free(fakeHead);
-	return flag;
 }
 
 int
