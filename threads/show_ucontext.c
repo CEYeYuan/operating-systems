987bec952aea4e92aed3d13edacb7fbedb67b8cd
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-10-11 14:32:55 -0400

    part 1

--- threads/show_ucontext.c
+++ threads/show_ucontext.c
@@ -67,8 +67,8 @@ main(int argc, char **argv)
 	       (unsigned long)(mycontext.uc_mcontext.gregs[REG_RIP]));
 
 	/* now, think about parameters */
-	printf("argc = %d\n", -1);
-	printf("argv = %p\n", (void *)-1);
+	printf("argc = %d\n", (int)mycontext.uc_mcontext.gregs[REG_RDI]);
+	printf("argv = %p\n", (void *)mycontext.uc_mcontext.gregs[REG_RSI]);
 	/* QUESTIONS: how are these parameters passed into the main function? 
 	 * are there any saved registers in mycontext that store the parameter
 	 * values above. why or why not? Hint: Use gdb, and then run
@@ -80,23 +80,22 @@ main(int argc, char **argv)
 	 * stack grow up or down? What are the stack related data in
 	 * mycontext.uc_mcontext.gregs[]? */
 	printf("memory address of the variable setcontext_called = %p\n",
-	       (void *)-1);
+	       (void *)&setcontext_called);
 	printf("memory address of the variable err = %p\n",
-	       (void *)-1);
+	       (void *)&err);
 	printf("number of bytes pushed to the stack between setcontext_called "
-	       "and err = %ld\n", (unsigned long)-1);
+	       "and err = %ld\n", (unsigned long)&setcontext_called-(unsigned long)&err);
 
 	printf("stack pointer register (RSP) stored in mycontext = 0x%lx\n",
-	       (unsigned long)-1);
+	       (unsigned long)mycontext.uc_mcontext.gregs[REG_RSP]);
 
 	printf("number of bytes between err and the saved stack in mycontext "
-	       "= %ld\n", (unsigned long)-1);
+	       "= %ld\n", (unsigned long)&err-(unsigned long)mycontext.uc_mcontext.gregs[REG_RSI]);
 
 	/* QUESTIONS: what is the uc_stack field in mycontext? does it point
 	 * to the current stack pointer, top of the stack, bottom of the stack,
 	 * or none of the above? */
-	printf("value of uc_stack.ss_sp = 0x%lx\n",
-	       (unsigned long)-1);
+	printf("value of uc_stack.ss_sp = 0x%lx\n",(unsigned long)mycontext.uc_stack.ss_sp);
 
 	/* Don't move on to the next part of the lab until you know how to
 	 * change the stack in a context when you manipulate a context to create
