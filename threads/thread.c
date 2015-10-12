21bf0a1cd04c121c1d6f90ac94a3102a25bc15f3
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-10-11 20:10:07 -0400

    partial done, create the struct of thread

--- threads/thread.c
+++ threads/thread.c
@@ -5,20 +5,101 @@
 #include "interrupt.h"
 
 /* This is the thread control block */
-struct thread {
-	/* ... Fill this in ... */
+
+enum state{ 
+	running=1,
+	ready=2,
+	blocked=3
 };
 
+
+
+//information about a particular thread
+typedef struct thread {
+	/* a linked list of thread */
+	ucontext_t *mycontext;
+	enum state status;
+	Tid id;
+	struct thread *next;
+} thread;
+
+struct list {
+	/* you can define this struct to have whatever fields you want. */
+	struct thread *head;
+	int size;
+};
+
+int 
+thread_compare(struct thread *a,struct thread *b){
+	//compare the id of two thread; no duplicate id are allowed
+	if(a->id>b->id)
+		return 1;
+	else
+		return -1;
+}
+
+void
+add_thread(struct list *readyqueue, struct thread *thread)
+{
+	if(readyqueue->size==0){
+		readyqueue->head=malloc(sizeof(struct thread));
+		readyqueue->head=thread;
+	}
+	else{
+		struct 	thread *head,*tmp;
+		head=readyqueue->head;
+		while(head->next!=NULL&&thread_compare(head,thread)<0){
+			head=head->next;
+		}
+		if(head->next==NULL)
+			head->next=thread;
+		else{
+			tmp=head->next;
+			head->next=thread;
+			thread->next=tmp;
+		}
+	}
+	readyqueue->size+=1;
+}
+
+
+struct thread *current;
+struct list *readyQueue;
+int *arr;//mark the # of threads, 0 for not used, 1 for used
 void
 thread_init(void)
 {
 	/* your optional code here */
+	current=malloc(sizeof(struct thread));
+	readyQueue=malloc(sizeof(struct list));
+	current->mycontext=malloc(sizeof(ucontext_t));
+	getcontext(current->mycontext);
+	current->id=0;
+	current->status=running;
+	current->next=NULL;//this field should never get used.
+	readyQueue=malloc(sizeof (struct list));
+	readyQueue->size=0;
+	arr=malloc(sizeof(int)*THREAD_MAX_THREADS);
+	int i=0;
+	while(i<THREAD_MAX_THREADS){
+		arr[i]=0;
+		i++;
+	}
 }
 
 Tid
 thread_id()
 {
-	TBD();
+/* return the thread identifier of the currently running thread 
+The first thread in the system (before the first call to thread_create) should have a 
+thread identifier of 0. Your threads system should support the creation of a maximum of 
+THREAD_MAX_THREADS concurrent threads by a program (including the initial main thread). 
+Thus, the maximum value of the thread identifier should thus be THREAD_MAX_THREADS - 1 
+(since thread identifiers start from 0). Note that when a thread exits, its thread 
+identifier can be reused by another thread created later.*/
+
+	if(current!=NULL)
+		return current->id;
 	return THREAD_INVALID;
 }
 
@@ -32,7 +113,22 @@ thread_create(void (*fn) (void *), void *parg)
 Tid
 thread_yield(Tid want_tid)
 {
-	TBD();
+/* suspend calling thread and run the thread with identifier tid. The calling
+ * thread is put in the ready queue. tid can be identifier of any available
+ * thread or the following constants:
+ *
+ * THREAD_ANY:	   run any thread in the ready queue.
+ * THREAD_SELF:    continue executing calling thread, for debugging purposes.
+ *
+ * Upon success, return the identifier of the thread that ran. The calling
+ * thread does not see this result until it runs later. Upon failure, the
+ * calling thread continues running, and returns the following:
+ *
+ * THREAD_INVALID: identifier tid does not correspond to a valid thread.
+ * THREAD_NONE:    no more threads, other than the caller, are available to
+ *		   run. this can happen is response to a call with tid set to
+ *		   THREAD_ANY. */
+	//TBD();
 	return THREAD_FAILED;
 }
 
