b3b092370886d0a1c48647cdb94f782fe9713eb7
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-11-09 12:41:07 -0500

    lab4 done

---
+++ marker/webserver/server_thread.c
@@ -0,0 +1,99 @@
+#include "request.h"
+#include "server_thread.h"
+#include "common.h"
+
+struct server {
+	int nr_threads;
+	int max_requests;
+	int max_cache_size;
+	/* add any other parameters you need */
+};
+
+/* static functions */
+
+/* initialize file data */
+static struct file_data *
+file_data_init(void)
+{
+	struct file_data *data;
+
+	data = Malloc(sizeof(struct file_data));
+	data->file_name = NULL;
+	data->file_buf = NULL;
+	data->file_size = 0;
+	return data;
+}
+
+/* free all file data */
+static void
+file_data_free(struct file_data *data)
+{
+	free(data->file_name);
+	free(data->file_buf);
+	free(data);
+}
+
+static void
+do_server_request(struct server *sv, int connfd)
+{
+	int ret;
+	struct request *rq;
+	struct file_data *data;
+
+	data = file_data_init();
+
+	/* fills data->file_name with name of the file being requested */
+	rq = request_init(connfd, data);
+	if (!rq) {
+		file_data_free(data);
+		return;
+	}
+	/* reads file, 
+	 * fills data->file_buf with the file contents,
+	 * data->file_size with file size. */
+	ret = request_readfile(rq);
+	if (!ret)
+		goto out;
+	/* sends file to client */
+	request_sendfile(rq);
+out:
+	request_destroy(rq);
+	file_data_free(data);
+}
+
+/* entry point functions */
+
+struct server *
+server_init(int nr_threads, int max_requests, int max_cache_size)
+{
+	struct server *sv;
+
+	sv = Malloc(sizeof(struct server));
+	sv->nr_threads = nr_threads;
+	sv->max_requests = max_requests;
+	sv->max_cache_size = max_cache_size;
+
+	if (nr_threads > 0 || max_requests > 0 || max_cache_size > 0) {
+		TBD();
+	}
+
+	/* Lab 4: create queue of max_request size when max_requests > 0 */
+
+	/* Lab 5: init server cache and limit its size to max_cache_size */
+
+	/* Lab 4: create worker threads when nr_threads > 0 */
+
+	return sv;
+}
+
+void
+server_request(struct server *sv, int connfd)
+{
+	if (sv->nr_threads == 0) { /* no worker threads */
+		do_server_request(sv, connfd);
+	} else {
+		/*  Save the relevant info in a buffer and have one of the
+		 *  worker threads do the work. */
+		TBD();
+	}
+}
