8c4a12b3bc0a716b7231b6d640d1b873a41bfb4f
CEYeYuan <eceye.yuan@mail.utoronto.ca>
2015-11-09 13:45:59 -0500

    all done

--- marker/os-156/webserver/server_thread.c
+++ marker/os-156/webserver/server_thread.c
@@ -9,6 +9,8 @@ struct server {
 	/* add any other parameters you need */
 };
 
+
+
 /* static functions */
 
 /* initialize file data */
@@ -61,8 +63,34 @@ out:
 	file_data_free(data);
 }
 
-/* entry point functions */
+/*GLOBAL VARIABLES*/
+pthread_t **threads;
+int *data;
+int in,out;
+pthread_mutex_t lock;
+pthread_cond_t full;
+pthread_cond_t empty;
+int buffer_size;
 
+
+void *
+stub(void *server){
+	while(1){
+		pthread_mutex_lock(&lock);
+		while(in==out)
+			pthread_cond_wait(&empty,&lock);
+		if((in-out+buffer_size)%buffer_size==buffer_size-1)
+			pthread_cond_signal(&full);
+		int conn=data[out];
+		out=(out+1)%buffer_size;
+		pthread_mutex_unlock(&lock);
+		do_server_request(server,conn);
+	}
+}
+/*pthread_create, pthread_mutex_init, pthread_mutex_lock, pthread_mutex_unlock, pthread_cond_init, pthread_cond_wait, and pthread_cond_signal*/
+
+
+/* entry point functions */
 struct server *
 server_init(int nr_threads, int max_requests, int max_cache_size)
 {
@@ -74,18 +102,39 @@ server_init(int nr_threads, int max_requests, int max_cache_size)
 	sv->max_cache_size = max_cache_size;
 
 	if (nr_threads > 0 || max_requests > 0 || max_cache_size > 0) {
-		TBD();
+		
+		if(nr_threads>=1){
+			data=malloc(sizeof(int)*(max_requests+1));
+			in=0;
+			out=0;
+			buffer_size=max_requests+1;
+			pthread_mutex_init(&lock, 0);
+			//pthread_mutex_lock(&lock);
+			threads=malloc(sizeof(pthread_t)*(nr_threads));
+			pthread_cond_init(&empty, NULL);
+			pthread_cond_init(&full, NULL);
+			int i;
+			for(i=0;i<nr_threads;i++){
+            	threads[i]=(pthread_t *) malloc (sizeof(pthread_t));
+			    int rc;
+				rc=pthread_create(threads[i],NULL,&stub,sv);
+				if (rc){
+					 printf("ERROR; return code from pthread_create() is %d\n", rc);
+					 exit(-1);
+				 }
+			}		
 	}
-
+	
 	/* Lab 4: create queue of max_request size when max_requests > 0 */
 
 	/* Lab 5: init server cache and limit its size to max_cache_size */
 
 	/* Lab 4: create worker threads when nr_threads > 0 */
 
+	}
+	//pthread_mutex_unlock(&lock);
 	return sv;
 }
-
 void
 server_request(struct server *sv, int connfd)
 {
@@ -94,6 +143,15 @@ server_request(struct server *sv, int connfd)
 	} else {
 		/*  Save the relevant info in a buffer and have one of the
 		 *  worker threads do the work. */
-		TBD();
+		pthread_mutex_lock(&lock);
+		while((in-out+buffer_size)%buffer_size==buffer_size-1){
+			pthread_cond_wait(&full, &lock);//buffer is full; wait
+		}
+		data[in]=connfd;
+		if(in==out)//if previously, the buffer is empty, some consumer(thread) might be blocked because of no request
+			pthread_cond_signal(&empty);
+		in=(in+1)%buffer_size;
+		pthread_mutex_unlock(&lock);
+		
 	}
 }
